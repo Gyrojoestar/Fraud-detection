7cfcb6220b49934845980210f5e42cd10290cd4b
Gyrojoestar <alphaomg555@gmail.com>
2026-09-15 03:07:38 +0800

    added cleaning and splitting

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
-import pandas as pd
+from sqlalchemy import create_engine, text
+import urllib.parse
+import os
+from dotenv import load_dotenv
 
-df = pd.read_csv('creditcard.csv')
+load_dotenv()
+raw_password = os.getenv("DATABASE_PW")
+encoded_password = urllib.parse.quote_plus(raw_password)
 
-print(df.head(10))
+# Get connection string from supabase
+# Format: postgresql+psycopg2://user:password@host:port/dbname
+DATABASE_URL = f"postgresql+psycopg2://myuser:{encoded_password}@localhost:5432/mydatabase"
+# initialise the sqlalchemy engine
+engine = create_engine(DATABASE_URL)
+
+# Test the connection + error handling
+try:
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT version();"))
+        print("Connected successfully!")
+        print("PostgreSQL Version:", result.fetchone()[0])
+except Exception as e:
+    print(f"An error occurred: {e}")
--- eda.py
+++ eda.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
+import os
+
+random_state=42
 
 df = pd.read_csv('creditcard.csv')
 
@@ -12,3 +16,31 @@ print(df[df['Class'] == 0]['Amount'].describe())
 print("===Fraud===")
 print(df[df['Class'] == 1]['Amount'].sort_values(ascending=False).head(10))
 print(df[df['Class'] == 1]['Amount'].describe())
+
+X = df.drop(columns=["Class"])
+y = df["Class"]
+
+# use train test split to evenly split the data, keep = use to simulate real time transaction
+# add = add to supabase
+keep_1, add_1, keep_2, add_2 = train_test_split(X, y, test_size=0.5, random_state=random_state, stratify=y)
+
+keep_1 = keep_1.reset_index(drop=True)
+keep_2 = keep_2.reset_index(drop=True)
+add_1 = add_1.reset_index(drop=True)
+add_2 = add_2.reset_index(drop=True)
+# combine to make the full csv with class column
+keep = pd.concat([keep_1, keep_2], axis=1).reset_index(drop=True)
+add = pd.concat([add_1, add_2], axis=1).reset_index(drop=True)
+
+keep["Time"] = keep_1["Time"].astype(int)
+add["Time"] = add_1["Time"].astype(int)
+
+df_list = [keep, add]
+output_path = ["creditCardSupabase.csv", "creditCardRealTime.csv"]
+for i, df_list_elem in enumerate(df_list):
+    if not os.path.exists(output_path[i]):
+        df_list_elem.to_csv(output_path[i], index=False)
+        print(f"file {output_path[i]} was created successfully.")
+    else:
+        print(f"file {output_path[i]} already exist. No files were created or changed.")
+        
